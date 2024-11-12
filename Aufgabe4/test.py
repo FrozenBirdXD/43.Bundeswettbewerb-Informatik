c166f34bc2761772df3c680ab44c94e6c4e487ef
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2024-11-12 21:03:25 +0100

    display graph of krocket gates

---
+++ Aufgabe4/test.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+segments_read = []
+
+with open('krocket/beispielaufgaben/krocket3.txt', 'r') as file:
+    next(file)
+    for line in file:
+        segment = list(map(int, line.split()))
+        segments_read.append(segment)
+
+plt.figure(figsize=(8, 6))
+
+# Plot each line segment
+for segment in segments_read:
+    x1, y1, x2, y2 = segment
+    plt.plot([x1, x2], [y1, y2], marker='o') 
+
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Krocket Gates")
+plt.grid(True)
+
+plt.show()
