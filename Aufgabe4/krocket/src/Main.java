cb901181d6c7d905ec21894620d46e89bcf8ac1e
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2024-11-13 20:58:11 +0100

    fix bug, cleanup, use radius of ball

--- Aufgabe4/krocket/src/Main.java
+++ Aufgabe4/krocket/src/Main.java
@@ -5,41 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    private int ballRadius;
-    private int totalCountGates;
-    private Point leftMostIntersection;
-
     public static void main(String[] args) {
         Main main = new Main();
 
         String input = null;
         try {
             // Read input from file
-            input = Files.readString(Path.of("krocket/beispielaufgaben/krocket3.txt"));
+            input = Files.readString(Path.of("krocket/beispielaufgaben/krocket1.txt"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         List<Gate> gates = main.parseFile(input);
 
-        // TODO: First try big step percentages, if not found try smaller until
+        double stepPercent = 10;
+        for (int i = 0; i < 4; i++) {
+            stepPercent /= 10;
+            Gerade gerade = main.findRayIntersectAllGates(gates, stepPercent);
 
-        // TODO: Use Ball radius
+            if (gerade != null) {
 
-        System.out.println("Shortest Gate: " + main.findShortestSegment(gates));
-        System.out.println("Edge Segment: " + main.findEdgeSegment(gates));
+                double x = gerade.getStart().getX() - 1;
+                Point start = new Point(x, gerade.getPointAt(x).getY());
 
-        Gerade gerade = main.findRayIntersectAllGates(gates, 0.01);
-        Ray ray = main.getKrocketRay(gerade);
+                System.out.println("Es ist möglich mit einem Schlag alle Tore zu durchqueren.");
+                System.out.println("Der Schlag kann als Halbgerade mit dem Startpunkt: " + start + ", die durch den Punkt " + gerade.getDirectionPoint() + " verläuft, beschrieben werden.");
+                System.out.println(gerade.getDirectionVec() + ", bzw. Steigung der Halbgeraden: " + gerade.getSlope());
 
-        System.out.println("Left most intersection: " + main.getLeftMostIntersection());
-
-        if (ray == null) {
-            System.out.println(
-                    "Es ist nicht möglich, alle Tore mit nur einem Schlag zu durchqueren.");
-        } else {
-            System.out.println("Es war möglich mit folgendem Ray: " + ray);
+                return;
+            }
         }
+        System.out.println(
+                "Es ist nicht möglich, alle Tore in der richtigen Reihenfolge mit nur einem Schlag zu durchqueren.");
     }
 
     public Point calculateLineIntersectSegment(Gerade line, Gate segment) {
@@ -71,26 +68,13 @@ public class Main {
         return null;
     }
 
-    public Ray getKrocketRay(Gerade gerade) {
-        if (gerade == null) {
-            return null;
-        }
-        // -1 to not start inside of a gate
-        double x = this.leftMostIntersection.getX() - 1;
-        return new Ray(new Point(x, gerade.getPointAt(x).getY()), gerade.getDirectionPoint());
-    }
-
     public Gerade findRayIntersectAllGates(List<Gate> gates, double stepPercent) {
         // Wähle das Randsegment und das kürzeste Segment
         Gate edgeSegment = null;
         Gate shortestSegment = null;
-        if (gates.size() == 2) {
-            edgeSegment = gates.get(0);
-            shortestSegment = gates.get(1);
-        } else {
-            edgeSegment = findEdgeSegment(gates);
-            shortestSegment = findShortestSegment(gates);
-        }
+
+        edgeSegment = gates.get(0);
+        shortestSegment = gates.get(1);
 
         // Iteriere in kleinen Schritten entlang des Startsegments
         for (double stepStart = 0; stepStart <= 1; stepStart += stepPercent) {
@@ -108,7 +92,6 @@ public class Main {
                                 + stepDir * (shortestSegment.getEnd().getX() - shortestSegment.getStart().getX()),
                         shortestSegment.getStart().getY()
                                 + stepDir * (shortestSegment.getEnd().getY() - shortestSegment.getStart().getY()));
-                // Ray ray = new Ray(startPoint, directionPoint);
                 Gerade gerade = new Gerade(startPoint, directionPoint);
 
                 boolean intersectsAll = true;
@@ -118,11 +101,6 @@ public class Main {
                     if (intersectionSegmentGerade == null) {
                         intersectsAll = false;
                         break;
-                    }
-                    // Update left most intersection
-                    if (this.leftMostIntersection == null
-                            || intersectionSegmentGerade.getX() < this.leftMostIntersection.getX()) {
-                        this.leftMostIntersection = intersectionSegmentGerade;
                     }
                 }
                 if (intersectsAll) {
@@ -138,63 +116,22 @@ public class Main {
         // Save every line in Array of Strings
         String[] lines = input.split("\n");
         String[] firstLine = lines[0].trim().split(" ");
-        this.totalCountGates = Integer.parseInt(firstLine[0].trim());
-        this.ballRadius = Integer.parseInt(firstLine[1].trim());
+        double totalCountGates = Integer.parseInt(firstLine[0].trim());
+        double ballRadius = Integer.parseInt(firstLine[1].trim());
+        System.out.println("Ball Radius: " + ballRadius);
 
         // For every line split at space and save
         for (int i = 1; i <= totalCountGates; i++) {
             String[] parts = lines[i].trim().split(" ");
             Point one = new Point((double) Integer.parseInt(parts[0]), (double) Integer.parseInt(parts[1]));
             Point two = new Point((double) Integer.parseInt(parts[2]), (double) Integer.parseInt(parts[3]));
-            gates.add(new Gate(one, two));
+
+            Gate gate = new Gate(one, two);
+            // Compensate for radius of ball
+            gate.shrink(ballRadius);
+
+            gates.add(gate);
         }
         return gates;
-    }
-
-    // TODO: Not necessary
-
-    public Gate findEdgeSegment(List<Gate> gates) {
-        // Initialize bounding box values
-        double minX = Double.MAX_VALUE, maxX = Double.MIN_VALUE;
-        double minY = Double.MAX_VALUE, maxY = Double.MIN_VALUE;
-
-        // Find bounding box
-        for (Gate segment : gates) {
-            minX = Math.min(minX, Math.min(segment.getStart().getX(), segment.getEnd().getX()));
-            maxX = Math.max(maxX, Math.max(segment.getStart().getX(), segment.getEnd().getX()));
-            minY = Math.min(minY, Math.min(segment.getStart().getY(), segment.getEnd().getY()));
-            maxY = Math.max(maxY, Math.max(segment.getStart().getY(), segment.getEnd().getY()));
-        }
-
-        Gate seg = null;
-        // Identify segments that touch the bounding box
-        for (Gate segment : gates) {
-            if (segment.getStart().getX() == minX || segment.getStart().getX() == maxX ||
-                    segment.getStart().getY() == minY || segment.getStart().getY() == maxY ||
-                    segment.getEnd().getX() == minX || segment.getEnd().getX() == maxX ||
-                    segment.getEnd().getY() == minY || segment.getEnd().getY() == maxY) {
-                // Return the first segment found that touches the bounding box
-                seg = segment;
-            }
-        }
-        return seg;
-    }
-
-    public Gate findShortestSegment(List<Gate> segments) {
-        Gate shortestSegment = segments.get(0);
-        double minLength = shortestSegment.length();
-
-        for (Gate segment : segments) {
-            double length = segment.length();
-            if (length < minLength) {
-                minLength = length;
-                shortestSegment = segment;
-            }
-        }
-        return shortestSegment;
-    }
-
-    public Point getLeftMostIntersection() {
-        return this.leftMostIntersection;
     }
 }
