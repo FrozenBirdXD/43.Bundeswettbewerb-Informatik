030a902655ed28bed203d6dec293b9d8a9bdb8f2
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2025-04-21 20:13:38 +0200

    move classes

--- Runde2/Aufgabe1/schmucknachrichten/src/TeilaufgabeA.java
+++ Runde2/Aufgabe1/schmucknachrichten/src/TeilaufgabeA.java
@@ -7,40 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-// Node for Huffman tree
-class Node implements Comparable<Node> {
-    Character character; // null for internal nodes
-    long frequency;
-    List<Node> children;
-    boolean isLeaf;
-
-    // For leaf nodes
-    public Node(Character character, long frequency) {
-        this.character = character;
-        this.frequency = frequency;
-        this.children = new ArrayList<>();
-        this.isLeaf = true;
-    }
-
-    // For internal nodes
-    public Node(List<Node> children, long frequency) {
-        this.character = null;
-        this.frequency = frequency;
-        this.children = children;
-        this.isLeaf = false;
-    }
-
-    @Override
-    public int compareTo(Node other) {
-        // Compare based on frequency, lower frequency first
-        return Long.compare(this.frequency, other.frequency);
-    }
-
-    public boolean isLeaf() {
-        return isLeaf;
-    }
-}
-
 // Implements n-ary Huffman coding
 public class TeilaufgabeA {
     public static void main(String[] args) {
