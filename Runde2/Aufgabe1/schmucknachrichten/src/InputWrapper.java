ef087e32728fae5247f4246a8f91e154e5ae87e5
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2025-04-21 16:14:57 +0200

    add diameters

--- Runde2/Aufgabe1/schmucknachrichten/src/InputWrapper.java
+++ Runde2/Aufgabe1/schmucknachrichten/src/InputWrapper.java
@@ -1,9 +1,13 @@
+import java.util.List;
+
 public class InputWrapper {
     int numColors;
     String text;
+    List<Integer> diameters;
 
-    public InputWrapper(int numColors, String text) {
+    public InputWrapper(int numColors, String text, List<Integer> diameters) {
         this.numColors = numColors;
         this.text = text;
+        this.diameters = diameters;
     }
 }
